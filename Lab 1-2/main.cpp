d51a59c985d171eed39e9f8396f02891e84c676a
unta1337 <firstapple42@gmail.com>
2023-03-15 16:15:12 +0900

    Lab 1-2 추가

--- Lab 1-2/main.cpp
+++ Lab 1-2/main.cpp
@@ -2,6 +2,8 @@
 #include "common.h"
 #include "../DSTimer/DS_timer.h"
 
+#define MANUAL_DIST
+
 #define DECRYPT_ALGORITHM(a,b,c) (c += a*50 + b)
  
 uchar* readData(char* _fileName, int _dataSize);
@@ -12,8 +14,8 @@ int main(int argc, char** argv)
 {
 	DS_timer timer(2, 1);
 
-	timer.setTimerName(0, "Serial Algorithm");
-	timer.setTimerName(1, "Parallel Algorithm");
+	timer.setTimerName(0, (char*)"Serial Algorithm");
+	timer.setTimerName(1, (char*)"Parallel Algorithm");
 
 	if (argc < 6) {
 		printf("It requires five arguments\n");
@@ -51,11 +53,56 @@ int main(int argc, char** argv)
 	// Hint: DECRYPT_ALGORITHM(A[i], B[i], parallelC[i])
 
 	// 2. Parallel algorithm
+	const int num_t = 20;							// 사용할 쓰레드 개수.
 
+#ifdef MANUAL_DIST
+	// 방법 1:
+	// 수동으로 데이터 분배.
+	// 
+	// 이미지는 R * C의 2차원으로 구성되므로, 각 쓰레드마다 일정 개수의 행을 부여.
+	// e.g.
+	//      6000 * 4000 이미지, 20개의 쓰레드 -> 각 쓰레드가 6000 / 20 = 300개의 행을 담당.
+	//      각 픽셀의 정보가 RGB로 세 개씩이므로 각 쓰레드가 300 * 4000 * 3개의 데이터를 담당.
 
-	/* Write your code from here! */
+	// 담당 행 분배.
+	const int unit_row_count = height / num_t;		// 한 개의 쓰레드가 담당할 행의 개수.
 
+	// 구간 분배.
+	int t_begin[num_t];
+	int t_end[num_t];
 
+	for (int i = 0; i < num_t; i++)
+	{
+		t_begin[i] = unit_row_count * width * 3 * i;
+		t_end[i] = unit_row_count * width * 3 * (i + 1) - 1;
+	}
+
+	// 행을 분배한 후, 남은 부분은 마지막 쓰레드가 담당.
+	t_end[num_t - 1] = dataSize - 1;
+
+	printf("[Workload Balance]\n");
+	for (int i = 0; i < num_t; i++)
+		printf("[Thread %d/%d] %.2f %%\n", i, num_t, ((double)t_end[i] - t_begin[i] + 1) / dataSize * 100);
+
+	#pragma omp parallel num_threads(num_t)
+	{
+		int t_num = omp_get_thread_num();
+		for (int i = t_begin[t_num]; i <= t_end[t_num]; i++) {
+			DECRYPT_ALGORITHM(A[i], B[i], parallelC[i]);
+		}
+	}
+#endif
+
+#ifndef MANUAL_DIST
+	// 방법 2:
+	// 자동으로 데이터 분배.
+	// omp parallel for 사용.
+
+	#pragma omp parallel for num_threads(num_t)
+	for (int i = 0; i < dataSize; i++) {
+		DECRYPT_ALGORITHM(A[i], B[i], parallelC[i]);
+	}
+#endif
 
 	// **************************************************************
 	timer.offTimer(1);
@@ -64,7 +111,7 @@ int main(int argc, char** argv)
 	bool isCorrect = true;
 	for (int i = 0; i < dataSize; i++) {
 		if (serialC[i] != parallelC[i]) {
-			isCorrect = false;
+			isCorrect = false; 
 			break;
 		}
 	}
