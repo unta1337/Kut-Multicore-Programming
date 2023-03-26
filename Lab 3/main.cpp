236d3bb2749f6199df3ad540c8f86391897132c7
unta1337 <firstapple42@gmail.com>
2023-03-26 18:36:36 +0900

    Lab 3, 방법 3 제외 구현

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -8,41 +8,68 @@
 #define INIT_PARALLEL 1
 
 #define NUM_T 20
-#define NUM_SAMPLES (1024 * 1024 * 1024)
+#define NUM_SAMPLES (1024 * 1024)
+// #define NUM_SAMPLES (1024 * 1024 * 1024)
 #define RAND_FLOAT (rand() % 10000 / 1000.0f)
 
 std::vector<float> inputs(NUM_SAMPLES);
 std::vector<int> serial_bins(10, 0);
-std::vector<int> parallel_bins(10, 0);
+std::vector<int> p_lock_bins(10, 0);
+std::vector<int> p_local_lock_bins(10, 0);
+std::vector<int> p_linear_bins(10, 0);
+std::vector<int> p_dnc_bins(10, 0);
+
+omp_lock_t g_lock;
 
 void init_random_float();
 
 int main(void) {
     srand((unsigned int)time(NULL));
 
-    DS_timer timer(3);
-    timer.setTimerName(0, (char*)"Init");
-    timer.setTimerName(1, (char*)"Serial");
-    timer.setTimerName(2, (char*)"Parallel");
+    DS_timer timer(6);
+    timer.setTimerName(0, (char*)"Init                                                 ");
+    timer.setTimerName(1, (char*)"Version 0, Serial                                    ");
+    timer.setTimerName(2, (char*)"Version 1, Parallel with Lock                        ");
+    timer.setTimerName(3, (char*)"Version 2, Parallel with Local bins and Lock         ");
+    timer.setTimerName(4, (char*)"Version _, Parallel with Linear Gathering            ");
+    timer.setTimerName(5, (char*)"Version 3, Parallel with Divide and Conqure Gathering");
 
-    // Init.
+    // 0. Init.
     timer.onTimer(0);
 
     init_random_float();
 
     timer.offTimer(0);
 
-    // Serial.
+    // 1. Serial.
     timer.onTimer(1);
 
     for (float f : inputs)
-        serial_bins[static_cast<int>(std::floor(f))]++;
+        serial_bins[static_cast<int>(f)]++;
 
     timer.offTimer(1);
 
-    // Parallel.
+    // 2. Parallel with Lock.
     timer.onTimer(2);
-    std::vector<std::vector<int>> local_bins(NUM_T, std::vector<int>(10, 0));
+
+    omp_init_lock(&g_lock);
+
+    #pragma omp parallel for num_threads(NUM_T)
+    for (size_t i = 0; i < NUM_SAMPLES; i++)
+    {
+        omp_set_lock(&g_lock);
+        p_lock_bins[static_cast<int>(inputs[i])]++;
+        omp_unset_lock(&g_lock);
+    }
+
+    timer.offTimer(2);
+
+    // 3. Parallel with Local bins and Lock.
+    timer.onTimer(3);
+
+    omp_init_lock(&g_lock);
+
+    std::vector<std::vector<int>> p_local_lock_local_bins(NUM_T, std::vector<int>(10, 0));
 
     #pragma omp parallel num_threads(NUM_T)
     {
@@ -50,35 +77,68 @@ int main(void) {
 
         #pragma omp for
         for (size_t i = 0; i < NUM_SAMPLES; i++)
-            local_bins[t_num][static_cast<int>(std::floor(inputs[i]))]++;
-    }
+            p_local_lock_local_bins[t_num][static_cast<int>(inputs[i])]++;
 
-    for (auto& local_bin : local_bins)
-        for (int i = 0; i < 10; i++)
-            parallel_bins[i] += local_bin[i];
-
-    timer.offTimer(2);
-
-    // Result.
-    bool is_correct = true;
-
-    for (size_t i = 0; i < 10; i++) {
-        if (serial_bins[i] != parallel_bins[i]) {
-            is_correct = false;
-            break;
+        #pragma omp for
+        for (int i = 0; i < NUM_T; i++) {
+            for (size_t j = 0; j < 10; j++) {
+                omp_set_lock(&g_lock);
+                p_local_lock_bins[j] += p_local_lock_local_bins[i][j];
+                omp_unset_lock(&g_lock);
+            }
         }
     }
 
-    std::cout << "The result is " << (is_correct ? "correct" : "incorrect.") << "\n\n";
+    timer.offTimer(3);
 
-    std::cout << "Serial Result:\n";
-    for (size_t i = 0; i < 10; i++)
-        std::cout << "[" << i << ", " << i + 1 << "): " << serial_bins[i] << "\n";
+    // 4. Parallel with Linear Gathering.
+    timer.onTimer(4);
+
+    std::vector<std::vector<int>> p_linear_local_bins(NUM_T, std::vector<int>(10, 0));
+
+    #pragma omp parallel num_threads(NUM_T)
+    {
+        int t_num = omp_get_thread_num();
+
+        #pragma omp for
+        for (size_t i = 0; i < NUM_SAMPLES; i++)
+            p_linear_local_bins[t_num][static_cast<int>(inputs[i])]++;
+    }
+    for (auto& local_bin : p_linear_local_bins)
+        for (int i = 0; i < 10; i++)
+            p_linear_bins[i] += local_bin[i];
+
+    timer.offTimer(4);
+
+    // 5.  Parallel with Divide and Conqure Gathering.
+    timer.onTimer(5);
+    timer.offTimer(5);
+
+    // Result.
+    std::cout << "1. Serial:                                      ";
+    for (int i = 0; i < 10; i++)
+        std::cout << i << ": " << serial_bins[i] << " | ";
     std::cout << "\n";
 
-    std::cout << "Parallel Result:\n";
-    for (size_t i = 0; i < 10; i++)
-        std::cout << "[" << i << ", " << i + 1 << "): " << parallel_bins[i] << "\n";
+    std::cout << "2. Parallel with Lock:                          ";
+    for (int i = 0; i < 10; i++)
+        std::cout << i << ": " << p_lock_bins[i] << " | ";
+    std::cout << "\n";
+
+    std::cout << "3. Parallel with Local bins and Lock:           ";
+    for (int i = 0; i < 10; i++)
+        std::cout << i << ": " << p_local_lock_bins[i] << " | ";
+    std::cout << "\n";
+
+    std::cout << "4. Parallel with Linear Gathering:              ";
+    for (int i = 0; i < 10; i++)
+        std::cout << i << ": " << p_linear_bins[i] << " | ";
+    std::cout << "\n";
+
+    std::cout << "5. Parallel with Divide and Conqure Gatheriung: ";
+    for (int i = 0; i < 10; i++)
+        std::cout << i << ": " << p_dnc_bins[i] << " | ";
+    std::cout << "\n";
 
     timer.printTimer();
 
